694367f9ce1c96815a72936bb8bd068ca168f720
nicooo130307 <tu_correo@ejemplo.com>
2026-05-23 23:10:45 -0600

    App base antes de cagarla

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,13 @@ plugins {
 
 android {
     namespace = "com.example.petkarnet"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 34
+
 
     defaultConfig {
         applicationId = "com.example.petkarnet"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
