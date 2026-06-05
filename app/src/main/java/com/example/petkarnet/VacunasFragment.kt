f72322731789ccc5a70f3a15b2b56a1426f4911b
nicooo130307 <tu_correo@ejemplo.com>
2026-06-05 14:22:56 -0600

    Fragment de vacunas

--- app/src/main/java/com/example/petkarnet/VacunasFragment.kt
+++ app/src/main/java/com/example/petkarnet/VacunasFragment.kt
@@ -95,7 +95,6 @@ class VacunasFragment : Fragment() {
                     Toast.makeText(requireContext(), "Error API: Código ${respuestaHistorial.code()}", Toast.LENGTH_LONG).show()
                 }
                 val historialReal = if (respuestaHistorial.isSuccessful) respuestaHistorial.body() ?: emptyList() else emptyList()
-                Toast.makeText(requireContext(), "Vacunas descargadas: ${historialReal.size}", Toast.LENGTH_LONG).show()
                 // 4. LA MAGIA: Cruzamos la lista ideal con el historial real
                 val listaSellos = vacunasBase.map { nombreIdeal ->
                     val registroEncontrado = historialReal.find {
